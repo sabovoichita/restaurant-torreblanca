bbce4207474f67b8ebed4acdac8d84f9a17d5ebb
Voichita Maria <sabo_voichita@yahoo.com>
2025-04-14 11:33:39 +0200

    delete old code and add 1 logo image

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Image
+        src="/image/logo/truscai-logo.png"
+        width={100}
+        height={100}
+        alt=""
+      />
       Homepage
     </>
   );
