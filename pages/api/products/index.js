af8cd4281f6445bf658631dd4b9300321965965b
Voichita Maria <sabo_voichita@yahoo.com>
2025-04-14 15:55:57 +0200

    add try catch for errors

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -8,7 +8,12 @@ export default async function handler(req, res) {
 
   if (method === "GET") {
   }
-
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   if (method === "POST") {
     try {
       const product = await Product.create(req.body);
