4f010ad020347929ec4878c75521a3ef20df4432
Voichita Maria <sabo_voichita@yahoo.com>
2025-04-14 13:16:12 +0200

    add more pages to nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -21,9 +21,11 @@ const Navbar = () => {
       </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
-          <li className={styles.listItem}>Orders</li>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
