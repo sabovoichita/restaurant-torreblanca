9a0501c60dcfb498b4a84b6109cf005746b5bc54
Voichita Maria <sabo_voichita@yahoo.com>
2025-04-15 15:02:29 +0200

    create cartSlice and store.js

---
+++ redux/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    products: [],
+    total: 0,
+  },
+  reducers: {
+    addProduct: (state, action) => {
+      state.products.push(action.payload);
+      state.total += action.payload.price * action.payload.quantity;
+    },
+    reset: (state) => {
+      state = initialState;
+    },
+  },
+});
+
+export const { addProduct, reset } = cartSlice.actions;
+export default cartSlice.reducer;
