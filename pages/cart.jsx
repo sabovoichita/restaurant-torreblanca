003dc551fb49e50bc4600924333447d3efe2d526
Voichita Maria <sabo_voichita@yahoo.com>
2025-04-16 12:06:25 +0200

    add paypal as option to pay at checkout

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,8 +1,82 @@
 import styles from "../styles/Cart.module.css";
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
 
 const Cart = () => {
+  // This value is from the props in the UI
+  const style = { layout: "vertical" };
+
+  function createOrder() {
+    // replace this url with your server
+    return fetch(
+      "https://react-paypal-js-storybook.fly.dev/api/paypal/create-order",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // use the "body" param to optionally pass additional order information
+        // like product ids and quantities
+        body: JSON.stringify({
+          cart: [
+            {
+              sku: "1blwyeo8",
+              quantity: 2,
+            },
+          ],
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((order) => {
+        // Your code here after create the order
+        return order.id;
+      });
+  }
+  function onApprove(data) {
+    // replace this url with your server
+    return fetch(
+      "https://react-paypal-js-storybook.fly.dev/api/paypal/capture-order",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          orderID: data.orderID,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((orderData) => {
+        // Your code here after capture the order
+      });
+  }
+
+  // Custom component to wrap the PayPalButtons and show loading spinner
+  const ButtonWrapper = ({ showSpinner }) => {
+    const [{ isPending }] = usePayPalScriptReducer();
+
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[style]}
+          fundingSource={undefined}
+          createOrder={createOrder}
+          onApprove={onApprove}
+        />
+      </>
+    );
+  };
+
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   return (
@@ -70,6 +144,16 @@ const Cart = () => {
             {cart.total}
           </div>
           <button className={styles.button}>CHECKOUT NOW!</button>
+          <PayPalScriptProvider
+            options={{
+              clientId: "test",
+              components: "buttons",
+              currency: "EURO",
+              disableFunding: "credit,card,p24",
+            }}
+          >
+            <ButtonWrapper showSpinner={false} />
+          </PayPalScriptProvider>
         </div>
       </div>
     </div>
