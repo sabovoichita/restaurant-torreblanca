8b114175dccc882b8c32b84988eb03310e66b6e5
Voichita Maria <sabo_voichita@yahoo.com>
2025-05-12 08:30:57 +0200

    small edit

--- components/OrderDetail.jsx
+++ components/OrderDetail.jsx
@@ -4,9 +4,11 @@ import styles from "../styles/OrderDetail.module.css";
 const OrderDetail = ({ total, createOrder }) => {
   const [customer, setCustomer] = useState("");
   const [address, setAddress] = useState("");
+
   const handleClick = () => {
     createOrder({ customer, address, total, method: 0 });
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
